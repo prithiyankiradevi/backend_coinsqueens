46e7aff01bbbd3a8fe0de03e56215af2947cb754
prithiyankiradevi <prithiyankiradevi.s@firebeetechnoservices.com>
2022-04-08 15:12:46 +0530

    08/04/2022

--- server.js
+++ server.js
@@ -50,6 +50,4 @@ app.use('/uploads',express.static('uploads'))
 
 // set port, listen for requests
 // const PORT = process.env.PORT || 8080;
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}.`);
-});
+app.listen()
